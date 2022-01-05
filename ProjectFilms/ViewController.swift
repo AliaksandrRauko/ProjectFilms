fa3ac69ae9fc272c5c51606c5bbb3aed423b41e9
AliaksandrRauko <ravkoaal@mail.ru>
2022-01-05 09:47:39 +0300

    Initial Commit

---
+++ ProjectFilms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectFilms
+//
+//  Created by Aliaksandr Rauko on 5.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
